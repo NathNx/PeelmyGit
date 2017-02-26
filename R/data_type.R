f8ab36b7b234ce12f8341c427a1a8eb75d2b0bd4
NattN <NaNxT@users.noreply.github.com>
2017-02-27 00:28:10 +0700

    Update data_type.R

--- R/data_type.R
+++ R/data_type.R
@@ -52,16 +52,22 @@ C
 #### Atomic ####
 
 # create dummy atomic
-A <- 2017
-B <- 'My name is Py'
-C <- 20L
-D <- "Z"
+A <- 2017 #numeric
+B <- 'My name is Py' #character
+C <- 20L #integer
+D <- "Z" #character
+E <- TRUE #logic
+F <- F #logic
+G <- 3.3.2 #double
 
 # how atomic looks like
 A
 B
 C
 D
+E
+F
+G
 
 
 
